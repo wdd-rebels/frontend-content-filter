442ccbcf80cdad90e25652836dc4f4aed4a10e7f
Kenneth Monteiro <kmonteiro@homeaway.com>
2019-03-15 15:16:21 +0000

    integration to /classify endpoint done with successful response from endpoint

--- contentScript.js
+++ contentScript.js
@@ -14,7 +14,7 @@ function getTweetData() {
 
     let allTweets = document.querySelectorAll('[data-item-type="tweet"]');
     console.log('length: ', Array.from(allTweets).length);
-    let tweetsToSend = {};
+    let tweetsToSend = [];
     Array.from(allTweets).forEach((tweet) => {
         if (tweet) {
             const tweetDetails = {
@@ -22,22 +22,30 @@ function getTweetData() {
                 text: tweet.querySelector('.tweet-text').innerText,
                 sender_handle: tweet.querySelector('.username').innerText
             }
-            console.log('tweetDetails: ', tweetDetails);
-            classify(tweetDetails);
+            tweetsToSend.push(tweetDetails);
         }
     });
+
+    var tweetPostBody = {
+        'tweets': tweetsToSend
+    };
+    classify(JSON.stringify(tweetPostBody));
 }
 
+// post request {'tweets': [{'id': 53536626, 'text': 'hello', 'sender_handle': 'Trump'}]}
 function classify(postBody) {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", 'https://polar-thicket-18683.herokuapp.com/classify', true);
-    
+
     //Send the proper header information along with the request
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onreadystatechange = function () {
         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
             var responseJson = JSON.parse(xhr.responseText);
             console.log("response from /classify", responseJson);
+        } else if (this.readyState === XMLHttpRequest.DONE && this.status !== 200) {
+            var errorJson = JSON.parse(xhr.responseText);
+            console.log("error response from /classify", errorJson);
         }
     }
     xhr.send(postBody);
