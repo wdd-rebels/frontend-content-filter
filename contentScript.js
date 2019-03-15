1dab737eac16a0b78d6e625cbbb00c37afd1476a
Becky Rush <becky.rush@bbc.co.uk>
2019-03-15 13:59:53 +0000

    add mutation observer to get tweets as loaded

--- contentScript.js
+++ contentScript.js
@@ -1,18 +1,36 @@
 'use strict';
 
-let tweets = document.getElementById('stream-items-id');
-console.log('tweets: ', tweets);
+const tweets = document.getElementById('stream-items-id');
+const config = { attributes: true, childList: true, subtree: true };
 
-let allTweets = document.querySelectorAll('[data-item-type="tweet"]');
-console.log('length: ', Array.from(allTweets).length);
-let tweetsToSend = {};
-Array.from(allTweets).forEach((tweet) => {
-    if (tweet) {
-        const tweetDetails = {
-            id: tweet.getAttribute('data-item-id'),
-            text: tweet.querySelector('.tweet-text').innerText,
-            sender_handle: tweet.querySelector('.username').innerText
+function getTweetData() {
+    console.log('getting tweets');
+    
+    console.log('tweets: ', tweets);
+
+    let allTweets = document.querySelectorAll('[data-item-type="tweet"]');
+    console.log('length: ', Array.from(allTweets).length);
+    let tweetsToSend = {};
+    Array.from(allTweets).forEach((tweet) => {
+        if (tweet) {
+            const tweetDetails = {
+                id: tweet.getAttribute('data-item-id'),
+                text: tweet.querySelector('.tweet-text').innerText,
+                sender_handle: tweet.querySelector('.username').innerText
+            }
+            console.log('tweetDetails: ', tweetDetails);
         }
-        console.log('tweetDetails: ', tweetDetails);
-    }
+    });
+}
+
+const insertedNodes = [];
+const observer = new WebKitMutationObserver((mutations) => {
+    // mutations.forEach(function (mutation) {
+    //     for (var i = 0; i < mutation.addedNodes.length; i++)
+    //         insertedNodes.push(mutation.addedNodes[i]);
+    // })
+    console.log('DOM changed');
+    getTweetData();
 });
+observer.observe(tweets, config);
+console.log(insertedNodes);
