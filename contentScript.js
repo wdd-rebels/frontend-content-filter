d81895e62bc356b4662f5ca79e39cb8c71b62fad
Becky Rush <becky.rush@bbc.co.uk>
2019-03-15 13:34:02 +0000

    grab some tweet details

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,17 @@
 'use strict';
 
-
 let tweets = document.getElementById('stream-items-id');
+console.log('tweets: ', tweets);
+
+let allTweets = document.querySelectorAll('[data-item-type="tweet"]');
+console.log('length: ', Array.from(allTweets).length);
+let tweetsToSend = {};
+Array.from(allTweets).forEach((tweet) => {
+    if (tweet) {
+        const tweetDetails = {
+            id: tweet.getAttribute('data-item-id'),
+            text: tweet.querySelector('.tweet-text').innerText
+        }
+        console.log('tweetDetails: ', tweetDetails);
+    }
+});
